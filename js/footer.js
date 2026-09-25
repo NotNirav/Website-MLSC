7d461eccfca5e90d9bd55717397ff7803765ffd8
KHUSHI KOLHE <8bkhushikolhe@gmail.com>
2026-09-25 15:01:25 +0530

    add shared site footer component script

--- js/footer.js
+++ js/footer.js
@@ -77,7 +77,7 @@
                 <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
                 <circle cx="12" cy="10" r="3" />
               </svg>
-              <span class="footer-address-text">Pimpri Chinchwad College of Engineering, Sector 26, Pradhikaran. Nigdi, Pune 411044</span>
+              <span class="footer-address-text">Pimpri Chinchwad College of Engineering, Sector 26, Pradhikaran, Nigdi, Pune 411044</span>
             </a>
           </div>`;
 
